f3df5cddf4c243e96b73f5a382d2dd8488098507
neswill1 <nenadkonatar@live.ac.me>
2015-04-17 09:52:54 +0200

    futer

--- first/index.php
+++ first/index.php
@@ -251,7 +251,7 @@
 		</div>
 		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
 
-            <h1>Powered by <a href="http://mobilegrows.com/">MobileGrows </a>|<a href="http://www.fivepointsdentistry.com/" rel="external"> Desktop Site</a></h1>
+            <h1>Powered by <a href="http://mobilegrows.com/" rel="external" >MobileGrows </a>|<a href="http://www.fivepointsdentistry.com/" rel="external"> Desktop Site</a></h1>
 		</div>
 		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
 		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
